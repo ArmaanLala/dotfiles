e381bfbbf0c460c33a54fe912df74f6c74b42c75
Armaan Lala <armaanlala@gmail.com>
2025-11-25 20:56:55 -0500

    what

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,29 +9,57 @@ if status is-interactive
     # fish_add_path $HOME/.local/bin
 end
 
-alias cd="z" 
-alias pman="sudo pacman -S" 
-alias c="clear" 
-alias v="nvim" 
-alias cat="bat" 
-alias grep="rg --color=auto" 
-alias shell="exec $SHELL -l" 
-alias fk="sudo !!" 
-alias mv="mv -i" 
-alias rm="rm -Iv" 
-alias df="df -h" 
-alias du="du -h -d 1" 
-alias k="killall" 
-alias p="ps aux | grep $1" 
-alias yay="paru" 
-alias lz="lazygit" 
+# Conditional aliases based on tool availability
+if type -q zoxide
+    alias cd="z"
+end
+
+alias pman="sudo pacman -S"
+alias c="clear"
+alias v="nvim"
+
+if type -q bat
+    alias cat="bat"
+end
+
+if type -q rg
+    alias grep="rg --color=auto"
+end
+
+
+if type -q distrobox
+    alias ds="distrobox"
+end
+
+alias fk="sudo !!"
+alias mv="mv -i"
+alias rm="rm -Iv"
+alias df="df -h"
+alias du="du -h -d 1"
+alias k="killall"
+alias p="ps aux | grep $1"
+alias yay="paru"
+alias lz="lazygit"
 alias i="$HOME/scripts/install.sh"
 alias mb="$HOME/scripts/microbin.sh"
-alias l="eza --color=auto --icons -h "
-alias ll="eza --color=auto --icons -lh"
-alias ls="eza --color=auto --icons -h"
-alias la="eza --color=auto --icons -lah"
 
-batman --export-env | source
-zoxide init fish | source
-starship init fish | source
+if type -q eza
+    alias l="eza --color=auto --icons -h "
+    alias ll="eza --color=auto --icons -lh"
+    alias ls="eza --color=auto --icons -h"
+    alias la="eza --color=auto --icons -lah"
+end
+
+
+# Initialize tools if available
+if type -q batman
+    batman --export-env | source
+end
+
+if type -q zoxide
+    zoxide init fish | source
+end
+
+if type -q starship
+    starship init fish | source
+end
