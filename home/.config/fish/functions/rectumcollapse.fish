92c181b8a7dbc5661b62c10e5396a88aa10abece
Armaan Lala <armaanlala@gmail.com>
2025-12-09 12:03:34 -0800

    Simplify fish config

--- home/.config/fish/functions/rectumcollapse.fish
+++
@@ -1,9 +0,0 @@
-function rectumcollapse
-    set -l counter 1
-    for file in (find . -mindepth 2 -type f | sort)
-        set -l ext (string match -r '\.[^.]+$' -- (basename $file) | string lower)
-        set -l newname (printf "%04d%s" $counter $ext)
-        mv -- $file $newname
-        set counter (math $counter + 1)
-    end
-end
