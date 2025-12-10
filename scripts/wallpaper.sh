d4c116734a6cad3b99959d2788dd0bc7aa7a5be2
Armaan Lala <armaanlala@gmail.com>
2025-12-10 01:07:20 -0800

    Fixing wallpaper script

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -8,7 +8,7 @@ if [ -n "$input_image" ] && [ -f "$input_image" ]; then
 	random_image="$input_image"
 else
 	cd "$folder" || exit
-	random_image=$(ls *.png *.jpg | shuf -n 1)
+	random_image=$(ls *.png *.jpg *.jpeg | shuf -n 1)
 fi
 
 # wal -i "$random_image" &
